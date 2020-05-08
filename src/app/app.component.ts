865250b007fe0b013ecc2b311a44ce607c698e9f
ab135790 <275284429@qq.com>
2020-05-08 18:04:37 +0800

    2020-5-8 first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TopMenu } from './components';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  scrollableTabBgColor = 'red'
   title = '拼多多';
+  topMenus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '手机',
+      link: ''
+    },
+    {
+      title: '食品',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '水果',
+      link: ''
+    },
+    {
+      title: '母婴',
+      link: ''
+    },
+    {
+      title: '美妆',
+      link: ''
+    },
+    {
+      title: '电器',
+      link: ''
+    },
+    {
+      title: '女装',
+      link: ''
+    },
+    {
+      title: '鞋包',
+      link: ''
+    },
+    {
+      title: '内衣',
+      link: ''
+    },
+    {
+      title: '家装',
+      link: ''
+    }
+  ];
+  handleTabSelect(topMenu: TopMenu) {
+    const colors = ['red', 'blue', 'black'];
+    const idx = Math.floor(Math.random() * 3)
+    this.scrollableTabBgColor = colors[idx];
+  }
 }
--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterContentInit, OnDestroy } from '@angular/core';
 
-interface TopMenu {
+export interface TopMenu {
   title: string;
   link: string;
 }
@@ -10,66 +10,88 @@ interface TopMenu {
   templateUrl: './scrollable-tab.component.html',
   styleUrls: ['./scrollable-tab.component.css']
 })
-export class ScrollableTabComponent implements OnInit {
+export class ScrollableTabComponent implements
+ OnInit,
+ OnChanges,
+ AfterContentInit,
+ AfterContentChecked,
+ AfterViewInit,
+ OnDestroy {
 
   selectedIndex = -1;
-  menus: TopMenu[] = [
-    {
-      title: '热门',
-      link: ''
-    },
-    {
-      title: '手机',
-      link: ''
-    },
-    {
-      title: '食品',
-      link: ''
-    },
-    {
-      title: '百货',
-      link: ''
-    },
-    {
-      title: '水果',
-      link: ''
-    },
-    {
-      title: '母婴',
-      link: ''
-    },
-    {
-      title: '美妆',
-      link: ''
-    },
-    {
-      title: '电器',
-      link: ''
-    },
-    {
-      title: '女装',
-      link: ''
-    },
-    {
-      title: '鞋包',
-      link: ''
-    },
-    {
-      title: '内衣',
-      link: ''
-    },
-    {
-      title: '家装',
-      link: ''
-    }
-  ];
-
+  @Input() menus: TopMenu[] = []; // 接收父级输入属性
+  @Input() backgroundColor = "";
+  @Input() titleColor = '';
+  @Input() titleAcitveColor = '';
+  @Input() indicatorColor = '';
+  @Output() tabSelected = new EventEmitter; // 向父级输出属性
   handleSelection(index: number) {
       this.selectedIndex = index;
+      this.tabSelected.emit(this.menus[this.selectedIndex]); // 向父级传输事件
   }
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {// 构造函数永远首先被调用
+    console.log('组件构造调用');
+    
   }
-
+  /**
+   * 在组件的`@Input` 属性发生变化的时候调用
+   * @param changes 索引对象，key 是属性的名字， value 是 SimpleChanges
+  */
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    console.log('组件输入属性改变', changes);
+  }
+  /**
+   * 组件初始化完成，在这个函数中，我们可以安全的使用组件的属性和方法
+  */
+  ngOnInit() { // 组件初始化时被调用
+    console.log('组件初始化');
+    
+  }
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log('组件脏值检测');
+  }
+  /**
+   * 组件内容初始化
+  */
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log('组件内容初始化');
+  }
+  /**
+   * 组件内容脏值检测
+  */
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    //Add 'implements AfterContentChecked' to the class.
+    console.log('组件内容脏值检测');
+  }
+  /**
+   * 组件的视图初始化
+  */
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log('组件视图初始化');
+  }
+  /**
+   * 组件视图脏值检测
+  */
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    console.log('组件视图脏值检测');
+  }
+  /**
+   * 需要做一些清理工作
+  */  
+ ngOnDestroy(): void {
+   //Called once, before the instance is destroyed.
+   //Add 'implements OnDestroy' to the class.
+  console.log('组件销毁');
+ }
 }
